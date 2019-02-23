aab2b2c548e00576683c357e0efd5e57edb950f7
Jamie Rushford <jar.rushford@gmail.com>
2019-02-23 12:11:09 -0700

    Set up UI and synonym fetching

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Vue.js App"/>
+    <Form v-bind="{ getSynonyms }" />
+    <ul>
+      <li
+        v-for="(syn, index) in synonyms"
+        v-bind:key="`syn-${index}`"
+      >
+        {{ syn }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Form from './components/Form.vue'
+  import { key } from './utils/apiKey.js'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      Form
+    },
+    data() {
+      return {
+        synonyms: [],
+        
+      }
+    },
+    methods: {
+      async getSynonyms(word) {
+        let url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${key}`
+
+        let response = await fetch(url)
+        if (response.ok) {
+          let result = await response.json()
+          if (typeof result[0] === 'string') {
+            this.$data.synonyms = result
+          } else {
+            result.forEach(blob => {
+              let synArrays = blob.meta.syns
+              synArrays.forEach(arr => {
+                this.$data.synonyms.push(...arr)
+              })
+            })
+          }
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h2>Enter a word below to find it's synonyms!</h2>
+    <input type="text" v-model="word" />
+    <button>Go</button>
+  </form>
+</template>
+
+
+<script>
+  export default {
+    name: 'Form',
+    props: ['getSynonyms'],
+    data() {
+      return {
+        word: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        let word = this.$data.word
+        this.$data.word = word
+        this.getSynonyms(word)
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
